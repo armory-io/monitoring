db6663221fcd8090f424910ce787c1a15233a550
andrewbackes <rbackes@ucla.edu>
2018-09-10 14:25:24 -0700

    Debug logging option

--- datadog/datadog.go
+++ datadog/datadog.go
@@ -17,6 +17,7 @@ type Monitor struct {
 	client *statsd.Client
 
 	logger logger
+	debug  bool
 }
 
 const (
@@ -75,6 +76,14 @@ func WithLogger(l logger) Option {
 	}
 }
 
+// Debug mode for the monitor.
+func Debug(name string) Option {
+	return func(m *Monitor) error {
+		m.debug = true
+		return nil
+	}
+}
+
 func newClient(host, port string) (*statsd.Client, error) {
 	h, p := host, port
 	if h == "" {
@@ -96,32 +105,48 @@ func (m *Monitor) log(format string, args ...interface{}) {
 	}
 }
 
+func (m *Monitor) error(err error) {
+	if m.debug == true && err != nil {
+		m.log("error: %v", err)
+	}
+}
+
 // Count can be incremented or decremented.
 func (m *Monitor) Count(name string, value int64, tags []string, rate float64) error {
 	m.log("Count{name: %s, value: %d, tags: %v, rate: %v}", name, value, tags, rate)
-	return m.client.Count(name, value, tags, rate)
+	err := m.client.Count(name, value, tags, rate)
+	m.error(err)
+	return err
 }
 
 // Decr a count.
 func (m *Monitor) Decr(name string, tags []string, rate float64) error {
 	m.log("DecrCount{name: %s, tags: %v, rate: %v}", name, tags, rate)
-	return m.client.Decr(name, tags, rate)
+	err := m.client.Decr(name, tags, rate)
+	m.error(err)
+	return err
 }
 
 // Incr a count.
 func (m *Monitor) Incr(name string, tags []string, rate float64) error {
 	m.log("IncrCount{name: %s, tags: %v, rate: %v}", name, tags, rate)
-	return m.client.Incr(name, tags, rate)
+	err := m.client.Incr(name, tags, rate)
+	m.error(err)
+	return err
 }
 
 // Event marks an event.
 func (m *Monitor) Event(title, text string) error {
 	m.log("Event{title: %s, text: %s}", title, text)
-	return m.client.SimpleEvent(title, text)
+	err := m.client.SimpleEvent(title, text)
+	m.error(err)
+	return err
 }
 
 // Gauge is used to set a metric to a specific value. It will stay at that value until changed.
 func (m *Monitor) Gauge(name string, value float64, tags []string, rate float64) error {
 	m.log("Gauge{name: %s, value: %d, tags: %v, rate: %v}", name, value, tags, rate)
-	return m.client.Gauge(name, value, tags, rate)
+	err := m.client.Gauge(name, value, tags, rate)
+	m.error(err)
+	return err
 }
